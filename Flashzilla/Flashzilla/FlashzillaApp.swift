2d579e5e4f9c44c341e7c278e92c65b2a767e9d9
DanoneYoghurt <83756452+DanoneYoghurt@users.noreply.github.com>
2024-07-24 13:50:48 +0300

    day 88 complete

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Антон Баскаков on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
