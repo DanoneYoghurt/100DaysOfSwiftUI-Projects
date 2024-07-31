2629f02ece7a35218798c52374cf924c44db7ae3
DanoneYoghurt <83756452+DanoneYoghurt@users.noreply.github.com>
2024-07-31 13:24:16 +0300

    day 98 complete

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var searchText = ""
+    @State private var favorites = Favorites()
+    
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
+    var filteredResorts: [Resort] {
+        if searchText.isEmpty {
+            resorts
+        } else {
+            resorts.filter { $0.name.localizedStandardContains(searchText) }
+        }
+    }
     
     var body: some View {
         NavigationSplitView {
-            List(resorts) { resort in
+            List(filteredResorts) { resort in
                 NavigationLink(value: resort) {
                     HStack {
                         Image(resort.country)
@@ -34,17 +45,27 @@ struct ContentView: View {
                             Text("\(resort.runs) runs")
                                 .foregroundStyle(.secondary)
                         }
+                        
+                        if favorites.contains(resort) {
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                                .accessibilityLabel("This is a favorite resort")
+                                .foregroundStyle(.red)
+                        }
                     }
                 }
                 
             }
+            
             .navigationTitle("Resorts")
             .navigationDestination(for: Resort.self) { resort in
                 ResortView(resort: resort)
             }
+            .searchable(text: $searchText, prompt: "Search for a resort")
         } detail: {
             WelcomeView()
         }
+        .environment(favorites)
     }
 }
 
---
+++ SnowSeeker/SnowSeeker/Favorites.swift
@@ -0,0 +1,39 @@
+//
+//  Favorites.swift
+//  SnowSeeker
+//
+//  Created by Антон Баскаков on 31.07.2024.
+//
+
+import SwiftUI
+
+@Observable
+class Favorites {
+    private var resorts: Set<String>
+    
+    private let key = "Favorites"
+    
+    init() {
+        
+        
+        resorts = []
+    }
+    
+    func contains(_ resort: Resort) -> Bool {
+        resorts.contains(resort.id)
+    }
+    
+    func add(_ resort: Resort) {
+        resorts.insert(resort.id)
+        save()
+    }
+    
+    func remove(_ resort: Resort) {
+        resorts.remove(resort.id)
+        save()
+    }
+    
+    func save() {
+        
+    }
+}
